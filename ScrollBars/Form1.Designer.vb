e340b8a2787d4579c53c81d23a09c71961d6ebd7
QuinicAcid <madica@yahoo.com>
2018-07-23 19:29:47 -0400

    Add project files.

---
+++ ScrollBars/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.picColor = New System.Windows.Forms.PictureBox()
+        Me.lblRed = New System.Windows.Forms.Label()
+        Me.hsbRed = New System.Windows.Forms.HScrollBar()
+        CType(Me.picColor, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Red"
+        '
+        'picColor
+        '
+        Me.picColor.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.picColor.Location = New System.Drawing.Point(633, 13)
+        Me.picColor.Name = "picColor"
+        Me.picColor.Size = New System.Drawing.Size(100, 103)
+        Me.picColor.TabIndex = 1
+        Me.picColor.TabStop = False
+        '
+        'lblRed
+        '
+        Me.lblRed.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblRed.AutoSize = True
+        Me.lblRed.Location = New System.Drawing.Point(576, 12)
+        Me.lblRed.Name = "lblRed"
+        Me.lblRed.Size = New System.Drawing.Size(0, 17)
+        Me.lblRed.TabIndex = 2
+        '
+        'hsbRed
+        '
+        Me.hsbRed.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.hsbRed.Location = New System.Drawing.Point(71, 12)
+        Me.hsbRed.Maximum = 264
+        Me.hsbRed.Name = "hsbRed"
+        Me.hsbRed.Size = New System.Drawing.Size(502, 21)
+        Me.hsbRed.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(745, 450)
+        Me.Controls.Add(Me.hsbRed)
+        Me.Controls.Add(Me.lblRed)
+        Me.Controls.Add(Me.picColor)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picColor, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents picColor As PictureBox
+    Friend WithEvents lblRed As Label
+    Friend WithEvents hsbRed As HScrollBar
+End Class
---
+++ ScrollBars/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub hsbRed_Scroll(sender As Object, e As ScrollEventArgs) Handles hsbRed.Scroll
+        lblRed.Text = hsbRed.Value
+    End Sub
+End Class
